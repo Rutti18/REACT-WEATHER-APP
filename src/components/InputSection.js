a05eec8aa24510a3957e2c7db5dd40366965479d
Rutti <ruttib@gmail.com>
2022-04-21 07:05:57 -0400

    first commit

---
+++ src/components/InputSection.js
@@ -0,0 +1,144 @@
+const InputSection = (data) => {
+    return (
+        <div className="requestDiv" >
+            <input type="text" name="zipCode" id="zipCode" placeholder="Enter Your Zip Code"/>
+            <button onClick={getLatLonByZipCode} > Submit </button>
+        </div>
+    );
+  }
+  
+  const AppId = '05418e642cd1a55786983cd93948ad16';
+
+  function getLatLonByZipCode(){
+    var ZipCode = document.getElementById("zipCode").value;
+    console.log(ZipCode)
+    if(ZipCode === ''){
+        alert('Enter a Zip Code !');
+    }else{
+        var url = 'https://api.openweathermap.org/geo/1.0/zip?zip='+ZipCode+'&appid='+AppId
+        // if (location.protocol === "http:"){
+        //     url = 'http://api.openweathermap.org/geo/1.0/zip?zip='+ZipCode+'&appid='+AppId
+        // }
+         fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+           if(data.cod === '404'){ 
+             alert(data.message);
+           }else{
+             document.getElementById('city').innerHTML = data.name;
+             let lat = data.lat;
+             let lon = data.lon; 
+             getWeatherReport(lat,lon);
+           }
+        });
+    }
+  }
+
+  
+
+  function getWeatherReport(lat,lon){
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&units=imperial&appid='+AppId)
+    .then(response => response.json())
+    .then(data => {
+        if(data.cod === '404'){
+            alert(data.message);
+        }else{
+            console.log(data);
+            let currentDate = new Date(data.current.dt * 1000);
+            var convertedDate = currentDate.toLocaleDateString('en-us', {
+                weekday: 'long',
+                day : 'numeric',
+                month : 'long',
+                year : 'numeric'
+            });
+
+
+        // time =   currentDate.toLocaleString('en-US', { hour: 'numeric', hour12: true })
+
+            
+            document.getElementById('currentDate').innerHTML = convertedDate; 
+            let currentTemp = data.current.temp+' <span>&#8457;</span>';
+            document.getElementById('currentTemp').innerHTML = currentTemp; 
+            let currentCondition = data.current.weather[0]['description'];
+            document.getElementById('currentContition').innerHTML = currentCondition; 
+                //01d@2x.png
+                console.log(data.current.weather[0]['icon'])
+            document.getElementById('sunny').src = '/icon/'+data.current.weather[0]['icon']+"@2x.png"; 
+            document.getElementById('sunny').classList.add('showImage');
+
+            document.getElementById('FeelsLike').innerHTML = data.current.feels_like+' <span>&#8457;</span>'; 
+            document.getElementById('Humidity').innerHTML = data.current.humidity+' <span>&#8457;</span>';
+
+
+
+            let TempHiLo = '';
+            let hourlyRecords = data.hourly;
+            let minTemp = ''; let maxTemp = '';
+            let time = "";
+
+            
+            for (let i=0 ; i<8 ; i++){
+                if(i === 0){
+                    minTemp =  hourlyRecords[i]['temp'];
+                }
+                if(i === 7){
+                    maxTemp = hourlyRecords[i]['temp'];
+                }
+            } 
+            TempHiLo = +maxTemp+' <span>&#8457;</span>' + ' / '+minTemp+' <span>&#8457;</span>';
+            document.getElementById('TempHiLow').innerHTML = TempHiLo; 
+
+            
+            getNextThreeDaysForeCast(data.daily);
+        }        
+    });
+  }
+
+  function getNextThreeDaysForeCast(dailyArray){
+
+    let DayOne = dailyArray[0];
+    let DayTwo = dailyArray[1];
+    let DayThree = dailyArray[2];
+    let currentDate = new Date(DayOne.dt * 1000);
+
+    let dayOneTemp = dailyArray[0]['temp']['min'] +' <span>&#8457;</span> ' + '/'+ dailyArray[0]['temp']['max'] +' <span>&#8457;</span> ';
+    let dayTwoTemp =  dailyArray[1]['temp']['min'] +' <span>&#8457;</span> '  +'/'+ dailyArray[1]['temp']['max'] +' <span>&#8457;</span> ';
+    let dayThreeTemp = dailyArray[2]['temp']['min'] +' <span>&#8457;</span> '  +'/'+ dailyArray[2]['temp']['max'] +' <span>&#8457;</span> ';
+
+    document.getElementById('dayOneTemp').innerHTML = dayOneTemp;
+    document.getElementById('dayTwoTemp').innerHTML = dayTwoTemp;
+    document.getElementById('dayThreeTemp').innerHTML = dayThreeTemp; 
+
+    var convertedDate = currentDate.toLocaleDateString('en-us', {
+        weekday: 'long'
+    });
+    document.getElementById('dayOneDate').innerHTML = convertedDate; 
+
+    currentDate = new Date(DayTwo.dt * 1000);
+    var convertedDate = currentDate.toLocaleDateString('en-us', {
+        weekday: 'long'
+    });
+    document.getElementById('dayTwoDate').innerHTML = convertedDate ;
+
+    currentDate = new Date(DayThree.dt * 1000);
+    var convertedDate = currentDate.toLocaleDateString('en-us', {
+        weekday: 'long'
+    });
+    document.getElementById('dayThreeDate').innerHTML = convertedDate  ;
+
+
+    console.log(DayOne.weather[0].description,DayTwo.weather[0].description,DayThree.weather[0].description)
+    document.getElementById('dayOneCond').innerHTML = DayOne.weather[0].description;
+    document.getElementById('dayTwoCond').innerHTML = DayTwo.weather[0].description;
+    document.getElementById('dayThreeCond').innerHTML = DayThree.weather[0].description;
+
+
+    document.getElementById('dayOneWeather').src = '/icon/'+DayOne.weather[0].icon+"@2x.png"; 
+    document.getElementById('dayTwoWeather').src = '/icon/'+DayTwo.weather[0].icon+"@2x.png"; 
+    document.getElementById('dayThreeWeather').src = '/icon/'+DayThree.weather[0].icon+"@2x.png"; 
+    
+    
+  }
+
+  export default InputSection;
---
+++ src/components/ReportSection.js
@@ -0,0 +1,66 @@
+const ReportSection = (data) => {
+    return (
+        <section className="reportSection">
+            <div className="first-container">
+                <table>
+                    <tbody>
+                        <tr>
+                        <td>
+                                <div className="reponseDiv">
+                                    <div className="current-city">
+                                        <h1><span> </span> <span className="city" id="city"></span></h1> <br/>
+                                    </div>
+                                    <div className="current-date">
+                                        <h3><span></span><span className="currentDate" id="currentDate"></span></h3> <br/>
+                                    </div>
+                                    <div className="temperature">
+                                        <h3><span></span><span className="FeelsLike" id="FeelsLike">  </span></h3>                                     
+                                    </div>
+                                    <div className="feelsLike">
+                                        <h4>feels like</h4>
+                                    </div>
+                                    <div className="humidity">
+                                        <h3><span></span><span className="Humidity" id="Humidity">  </span></h3>
+                                    </div>
+                                    <div className="Humidity">
+                                        <h4>humidity</h4>
+                                    </div>
+
+                                    <div className="current-temperature">
+                                        <h1><span></span><span className="currentTemp" id="currentTemp"></span></h1><br/>
+                                        <div></div>
+                                    </div>  
+                                    <div>
+                                        <img src=""  id="sunny" style={{display: "none"}}/>
+                                    </div>
+                                    <div className="current-conditions">
+                                        <h3><span></span><span className="currentContition" id="currentContition"></span> <span id="icon"> </span></h3><br/>
+                                    </div>                                                                         
+                                    <div className="temperature">
+                                        <h3><span></span><span className="TempHiLow" id="TempHiLow"> </span></h3><br/>
+                                    </div>
+                                    <div className="modReq">
+                                        <div className="day1" id="dayOne"> <br/> <span id="dayOneDate"> </span> <br/> <span id="dayOneTemp">  </span> <br/> <span id="dayOneCond"> </span>  </div>
+                                        <div className="day2" id="dayTwo"> <br/> <span id="dayTwoDate"> </span> <br/><span id="dayTwoTemp"> </span> <br/> <span id="dayTwoCond"> </span>  </div>
+                                        <div className="day3" id="dayThree"> <br/> <span id="dayThreeDate"> </span><br/> <span id="dayThreeTemp"></span> <br/> <span id="dayThreeCond"> </span> </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="weather-forecast"> 
+                                    <h1>WEATHER <br/> FORECAST</h1>
+                                </div>
+                            </td>     
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>  
+    );
+}
+  
+export default ReportSection;
+
+{/* <img src="" id="dayOneWeather"/>
+<img  src="" id="dayTwoWeather"/>
+<img src="" id="dayThreeWeather"/> */}
